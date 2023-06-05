90aaedb664f6028e1e1ddc56da7f24a7b0d98064
Manoj Kumar M R <manoj.mr@experionglobal.local>
2023-06-05 08:41:35 +0530

    initial commit

---
+++ src/main/java/com/school/greet/GreetWebappApplication.java
@@ -0,0 +1,13 @@
+package com.school.greet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/greet/GreetWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.greet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
