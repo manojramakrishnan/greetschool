76e8d792fdbbac06680b5017dd7a0ed9146fedd3
akhil murali <akhilmurali662@gmail.com>
2023-06-09 09:17:56 +0530

    added api index

--- src/main/java/com/school/greet/controller/HomeController.java
+++ src/main/java/com/school/greet/controller/HomeController.java
@@ -1,5 +1,22 @@
 package com.school.greet.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = {"/", "/index" }, method = RequestMethod.GET)
+	public ModelAndView homeScreen() {
+		System.err.println("inside default get method");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("school/index.html");
+		return modelAndView;
+	}
+
+	public HomeController() {
+		
+	}
 }
